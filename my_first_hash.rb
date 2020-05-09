067ec1b34623930c3545734e704a13932d38b571
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 02:53:09 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -10,12 +10,9 @@ def shipping_manifest
 end
 
 def retrieval
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  return shipping_manifest["oil paintings"]
+  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
+  return the_manifest["oil paintings"]
+
 end
 
 def adding
