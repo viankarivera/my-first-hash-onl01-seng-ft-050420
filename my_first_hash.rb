29039dec0986eaa3cc50b293ea1b84e851aadc1f
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 02:57:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,35 +1,4 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 my_hash = {"name" => "Vianka", "age" => "25", "pet" => "cat"}
-
-end
-
-
-def shipping_manifest
-  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
-end
-
-def retrieval
-  the_manifest = {"whale bone corsets" => "5", "porcelain vases" => "2", "oil paintings" => "3"}
-  the_manifest["oil paintings"]
-
-end
-
-def adding
-  shipping_manifest = {
-                  "whale bone corsets" => 5,
-                  "porcelain vases" => 2,
-                  "oil paintings" => 3
-                 }
-  # add 2 muskets to the shipping_manifest hash below
-    shipping_manifest["Muskets"] = "2"
-
-
-  # add 4 gun powder to the shipping_manifest hash below
-    shipping_manifest["gun powder"] = "4"
-
-
-  # return the shipping_manifest hash below
-  shipping_manifest
-
 end
