4824c3411148a4e77f6ab2059066aa1ff7c40e60
Vianka Rivera <viankalrivera@gmail.com>
2020-05-09 02:59:49 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -2,3 +2,6 @@ def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 my_hash = {"name" => "Vianka", "age" => "25", "pet" => "cat"}
 end
+
+def shipping_manifest
+  shipping_manifest = {"whale bone corsets" => "2", "oil paintings" => "5", "porcelain vases" => "3"}
